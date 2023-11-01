f66469275e35e6b17ec939a725739e6b8dec6536
Dario <dario_jav@hotmail.com>
2023-10-31 20:52:41 -0500

    descomentado de linea de control de ubicacion

--- ViewModel/FormSegAsisViewModel.cs
+++ ViewModel/FormSegAsisViewModel.cs
@@ -141,7 +141,7 @@ namespace Asis_Batia.ViewModel
                     if (count == 0)
                     {
                         count++;
-                        var result = true;// await DisplayAlert("Acción no permitida", "Parece que estas lejos de tu servicio, ¿Deseas registrarte en otro servicio?", "Si", "No");
+                        var result = await DisplayAlert("Acción no permitida", "Parece que estas lejos de tu servicio, ¿Deseas registrarte en otro servicio?", "Si", "No");
                         if (result)
                         {
                             var data = new Dictionary<string, object>
